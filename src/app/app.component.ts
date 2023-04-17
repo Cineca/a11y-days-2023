36e95848291c0b86faa8f3c1481e7d2cb871595c
g.bonomo <g.bonomo@cineca.it>
2023-04-17 12:49:36 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'demo-a11y-cineca';
+}
